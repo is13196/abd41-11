6cec223f8037aae2221ab7f1a041aa365d84b0bc
Slava <slavamoto2004@mail.ru>
2024-03-04 00:54:19 +0700

    first

--- abd41-11/Маршруты.cs
+++
@@ -1,8 +0,0 @@
-namespace abd41_11;
-
-public class Маршруты
-{
-    public int id { get; set; }
-    public string название { get; set; }
-    public float расстояние { get; set; }
-}
--- abd41-11/Транспортные_средства.cs
+++
@@ -1,9 +0,0 @@
-namespace abd41_11;
-
-public class Транспортные_средства
-{
-    public int id { get; set; }
-    public string название { get; set; }
-    public string тип { get; set; }
-    public int вместимость { get; set; }
-}
